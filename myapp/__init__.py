d0a7743d84264192b22bef66c5db8db5e5657d67
rafayel <rafayel.veziryan@sentium-consulting.com>
2024-06-20 00:59:24 +0400

    deleted all unused comments

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -1,7 +1,1 @@
-from flask import Flask
 from myapp import views
-import os
-
-### For Running Flask app with main method - python run.py ###
-# if os.environ.get('OPENAI_API_KEY') is None:
-#     os.environ['OPENAI_API_KEY'] = 'sk-proj-qNgaKQod1ZhL2hPwkF8jT3BlbkFJPErNwkQLQnxUxpzt4xzc'
